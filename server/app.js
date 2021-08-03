198a11c7630c7a19f72b6a9c0b704dffde993987
Hela <hela.chatti.landolsi@gmail.com>
2021-08-03 17:54:01 +0200

    add modification to deploy on heroku

--- server/app.js
+++ server/app.js
@@ -28,5 +28,14 @@ app.use('/', router);
 app.use('/posts', router);
 app.use('/:id',router);
 
+//serve static assets if in production
+if(process.env.NODE_ENV === 'production'){
+   //set static folder
+   app.use(express.static('client/build'));
+   app.get('*',(req,res) => {
+     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+   });
+  }   
+
 //start listening to the server
 app.listen(port, () => console.log(`server listening on ${port}`));

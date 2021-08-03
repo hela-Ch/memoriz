88ee07aeb0e14710a0c1ffaa5c3e0eba74328468
Hela <hela.chatti.landolsi@gmail.com>
2021-08-03 18:34:09 +0200

    modify to deploy on heroku

--- server/app.js
+++ server/app.js
@@ -31,9 +31,9 @@ app.use('/:id',router);
 //serve static assets if in production
 if(process.env.NODE_ENV === 'production'){
    //set static folder
-   app.use(express.static('client/build'));
+   app.use(express.static(path.resolve(__dirname, "./client/build")));
    app.get('*',(req,res) => {
-     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+     res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
    });
   }   
 
